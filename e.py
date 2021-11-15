a84d37c5e25a663963279d602f47221e26f1d17d
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2021-11-15 15:47:05 -0800

    Add files via upload

---
+++ e.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+import time
+import csv
+START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
+browser = webdriver.Chrome("/home/dev_veve/Downloads/C127/chromedriver_linux64/chromedriver")
+browser.get(START_URL)
+time.sleep(10)
+headers = ["Name", "Distance", "Mass", "Radius"]
+planet_data = []
+new_planet_data = []
+def scrape():
+    for i in range(1, 1):
+        while True:
+            time.sleep(2)
+            soup = BeautifulSoup(browser.page_source, "html.parser")
+            current_page_num = int(soup.find_all("input", attrs={"class", "page_num"})[0].get("value"))
+            if current_page_num < i:
+                browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+            elif current_page_num > i:
+                browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[1]/a').click()
+            else:
+                break
+
+
+scrape()
+
+final_planet_data = []
+for index, data in enumerate(planet_data):
+    new_planet_data_element = new_planet_data[index]
+    new_planet_data_element = [elem.replace("\n", "") for elem in new_planet_data_element]
+    new_planet_data_element = new_planet_data_element[:7]
+    final_planet_data.append(data + new_planet_data_element)
+with open("final.csv", "w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(final_planet_data)
